22735235aaf658320eeed9d9e4cef9bbfcc8d8e0
NewarkX <brunowmanha@gmail.com>
2022-05-11 11:19:04 -0300

    productfilter layout

--- front-web/src/components/ProductFilter/index.tsx
+++ front-web/src/components/ProductFilter/index.tsx
@@ -15,11 +15,7 @@ type ProductFilterData = {
 const ProductFilter = () => {
   const [selectCategories, setSelectCategories] = useState<Category[]>([]);
 
-  const {
-    register,
-    handleSubmit,
-    control,
-  } = useForm<ProductFilterData>();
+  const { register, handleSubmit, control } = useForm<ProductFilterData>();
 
   const onSubmit = (formData: ProductFilterData) => {};
 
@@ -40,7 +36,7 @@ const ProductFilter = () => {
             placeholder="Nome do produto"
             name="name"
           />
-          <button>
+          <button className="product-filter-search-icon">
             <SearchIcon />
           </button>
         </div>
@@ -54,14 +50,18 @@ const ProductFilter = () => {
                   {...field}
                   options={selectCategories}
                   isClearable
-                  classNamePrefix="product-crud-select"
+                  placeholder="Categoria"
+                  classNamePrefix="product-filter-select"
                   getOptionLabel={(category: Category) => category.name}
                   getOptionValue={(category: Category) => String(category.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary">LIMPAR</button>
+          <button className="btn btn-outline-secondary  btn-product-filter-clear">
+            LIMPAR
+            <span className="btn-product-filter-word"> FILTRO</span>
+          </button>
         </div>
       </form>
     </div>
