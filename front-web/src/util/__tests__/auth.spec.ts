6b147788ae5143888132287d6add1076d817a3d1
bruno <brunowmanha@gmail.com>
2022-05-23 10:49:18 -0300

    mocking function with spyOn

---
+++ front-web/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,22 @@
+import { hasAnyHoles } from '../auth';
+import * as TokenModule from '../token';
+
+describe('hasAnyHoles tests', () => {
+
+    test('should return true when empty list', () => {
+        const result = hasAnyHoles([]);
+        expect(result).toEqual(true);
+    })
+
+    test('should return true when user has given hole', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValueOnce({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN','ROLE_OPERATOR'],
+        })
+
+        const result = hasAnyHoles(['ROLE_ADMIN']);
+        expect(result).toEqual(true);
+    })
+})
